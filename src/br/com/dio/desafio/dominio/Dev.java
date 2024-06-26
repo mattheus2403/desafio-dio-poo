dc78bac0e23f6d74b1deae6dc167a72077720cb0
Mattheus2403 <matheusmoraidois@gmail.com>
2024-06-26 20:47:45 -0300

    feat: add class Bootcamp and Dev

---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,73 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+
+	//atributos
+	private String nome;
+	private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();	
+	
+	//inscrição
+	public void inscreverBootcamp(Bootcamp bootcamp) {
+		
+	}
+	
+	//progredir
+	public void progredir() {
+		
+	}
+
+	//calcula xp
+	public void calcularTotalXp() {
+		
+	}
+	
+	//getters e setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Set<Conteudo> getConteudosInscritos() {
+		return conteudosInscritos;
+	}
+
+	public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+		this.conteudosInscritos = conteudosInscritos;
+	}
+
+	public Set<Conteudo> getConteudosConcluidos() {
+		return conteudosConcluidos;
+	}
+
+	public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+		this.conteudosConcluidos = conteudosConcluidos;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(conteudosConcluidos, conteudosInscritos, nome);
+	}
+	
+	//equals e hashcode
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dev other = (Dev) obj;
+		return Objects.equals(conteudosConcluidos, other.conteudosConcluidos)
+				&& Objects.equals(conteudosInscritos, other.conteudosInscritos) && Objects.equals(nome, other.nome);
+	}
+	
+}
